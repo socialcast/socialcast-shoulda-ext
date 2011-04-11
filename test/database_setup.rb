29ab314d418d81410a8d4365ab610e0c3c334636
Geoffrey Hichborn <geoff@socialcast.com>
2011-04-10 18:04:08 -0700

    added tables to database_setup added .gitignore for test/debug.log and test/should_ext.sqlite3

--- test/database_setup.rb
+++ test/database_setup.rb
@@ -2,3 +2,23 @@ require 'logger'
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite'])
+
+ActiveRecord::Schema.define(:version => 1) do
+  create_table :blogs, :force => true do |t|
+    t.column :title, :string
+  end
+  create_table :comments, :force => true do |t|
+    t.column :blog_id, :integer
+    t.column :text, :string
+  end
+end
+
+class Blog < ActiveRecord::Base
+  has_many :comments, :dependent => :destroy
+  attr_accessible :title
+end
+
+class Comment < ActiveRecord::Base
+  attr_accessible :text
+  belongs_to :blog
+end
