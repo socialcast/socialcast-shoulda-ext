9f2230c50e1ea6cc303dfdf09c8d0eca4208f517
Geoffrey Hichborn <geoff@socialcast.com>
2011-04-11 00:26:05 -0700

    Added tests for trigger_callbacks matcher

--- test/test_trigger_callback_matcher.rb
+++ test/test_trigger_callback_matcher.rb
@@ -2,7 +2,37 @@
 require File.join(File.dirname(__FILE__), 'helper')
 
 class TestTriggerCallbackMatcher < Test::Unit::TestCase
-  def test_something
-    flunk "you need to add some tests"
+  
+  context "doing nothing to a record" do
+    subject { Blog.new :title => 'blog title' }
+    should_not trigger_callbacks
+  end
+  
+  context "creating a record" do
+    subject { Blog.create! :title => 'blog title' }
+    should trigger_callbacks.for :create
+    should_not trigger_callbacks.for :update, :destroy
+  end
+  
+  context "updating a record" do
+    subject do
+      Blog.create!( :title => 'blog title').tap do |b|
+        b.reset_callback_flags!
+        b.update_attribute :title, 'new blog title'
+      end
+    end
+    should trigger_callbacks.for :update
+    should_not trigger_callbacks.for :create, :destroy
+  end
+  
+  context "destroying a record" do
+    subject do
+      Blog.create!( :title => 'blog title').tap do |b|
+        b.reset_callback_flags!
+        b.destroy
+      end
+    end
+    should trigger_callbacks.for :destroy
+    should_not trigger_callbacks.for :create, :update
   end
 end
